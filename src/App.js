28dedd22a025db44b2aa479b1fb542c3f6af502e
zeyaam95 <zeyaam898@gmail.com>
2023-12-16 00:15:15 -0500

    - Added React Router Dom - Setup home route - Removed react boilerplate page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  document.title = "Zeyaam Shahid";
+  const defElement = (
+    <header className="App-header">
+      Coming soon
+    </header>
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={defElement}/>
+      </Routes>
     </div>
   );
 }
